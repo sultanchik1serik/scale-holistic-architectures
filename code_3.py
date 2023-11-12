926b7b0b6f088e719cf5643472b469d44e94cfb8
sultanchik1serik <142357734+sultanchik1serik@users.noreply.github.com>
2023-11-12 11:16:16 -0500

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,52 +1,52 @@
+import random
+
+        print(f"Random Number: {item}")
+def generate_random_data():
+if __name__ == "__main__":
 
     data = [random.randint(1, 100) for _ in range(10)]
-import random
-        print(f"Random Number: {item}")
-if __name__ == "__main__":
+
+    data = generate_random_data()
+
 def main():
     return data
     for item in data:
-def generate_random_data():
     main()
-
-
-    data = generate_random_data()
     return data
-if __name__ == "__main__":
 def main():
-
-    data = [random.randint(1, 100) for _ in range(10)]
-import random
 def generate_random_data():
     for item in data:
-
-
     main()
-    data = generate_random_data()
-        print(f"Random Number: {item}")
-    return data
-    data = [random.randint(1, 100) for _ in range(10)]
 
-        print(f"Random Number: {item}")
-
-    for item in data:
 import random
 if __name__ == "__main__":
-def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+
     data = generate_random_data()
+        for (int i = 0; i < 10; i++) {
 
 
+        }
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+        int[] data = new int[10];
+public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int item : data) {
+}
+    }
+        }def main():
     main()
-def main():if __name__ == "__main__":
-    for item in data:
+        print(f"Random Number: {item}")
 
-import random
+def generate_random_data():
     data = [random.randint(1, 100) for _ in range(10)]
     data = generate_random_data()
 
-    main()
-    return data
-def generate_random_data():
+    for item in data:
 
-def main():
-        print(f"Random Number: {item}")
+
+import random
+if __name__ == "__main__":
+    return data
